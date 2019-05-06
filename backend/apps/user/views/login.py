dc6c55cd9fe0df9097cf15f90db2762c9c4ee752
hizir_aslan <hzraslan@gmail.com>
2019-05-06 23:21:38 +0300

    login

--- backend/apps/user/views/login.py
+++ backend/apps/user/views/login.py
@@ -9,18 +9,16 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+import datetime
 @csrf_exempt
 def login(request):
-    # User.objects.all().delete()
-    # data = serializers.serialize('json', User.objects.all(), ensure_ascii=False)
-    # return HttpResponse(data, content_type="application/json")
     username = request.POST['username']
     password = request.POST['password']
     email = request.POST['email']
     user = authenticate(request, email=email, password=password)
     if user: 
-        token = jws.sign({'username': user.email}, 'pythonforum',  algorithm='HS256')
-        print(token)
+        expiry = str(datetime.date.today() + datetime.timedelta(days=50))
+        token = jws.sign({'username': user.email, 'expiry': expiry}, 'secret',  algorithm='HS256')
         return HttpResponse(json.dumps({'token':token, 'success': True}), content_type='application/json')
     else:
         return HttpResponse(json.dumps({'success': False}), content_type='application/json')
